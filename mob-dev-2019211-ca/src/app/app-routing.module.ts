316c453a1d8bb058fd62d1852a8edf8c74bc34ce
diogograve160390 <diogograve@icloud.com>
2020-04-14 00:30:45 +0000

    I changed some codes.

--- mob-dev-2019211-ca/src/app/app-routing.module.ts
+++ mob-dev-2019211-ca/src/app/app-routing.module.ts
@@ -2,42 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  },
-  {
-    path: 'characters-details',
-    loadChildren: () => import('./pages/characters-details/characters-details.module').then( m => m.CharactersDetailsPageModule)
-  },
-  {
-    path: 'episodes',
-    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
-  },
-  {
-    path: 'episodes-details',
-    loadChildren: () => import('./pages/episodes-details/episodes-details.module').then( m => m.EpisodesDetailsPageModule)
-  },
-  {
-    path: 'quotes',
-    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
-  },
-  {
-    path: 'death-count',
-    loadChildren: () => import('./pages/death-count/death-count.module').then( m => m.DeathCountPageModule)
-  },
+
+    { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule) }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
